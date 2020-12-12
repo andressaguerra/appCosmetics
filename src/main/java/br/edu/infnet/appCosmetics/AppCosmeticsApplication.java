0166ca107b8f066987e8225dff90bb257693773f
Andressa Guerra <andressa.guerra@al.infnet.edu.br>
2020-12-12 19:03:40 -0300

    Criacao do projeto appCosmetics

---
+++ src/main/java/br/edu/infnet/appCosmetics/AppCosmeticsApplication.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.appCosmetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCosmeticsApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(AppCosmeticsApplication.class, args);
+		System.out.println(args.length);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/appCosmetics/AppCosmeticsApplicationTests.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.appCosmetics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCosmeticsApplicationTests {
+
+	@Test
+	void testeInicialApp() {
+		AppCosmeticsApplication.main(new String[] {"anddie"});
+	}
+
+}
